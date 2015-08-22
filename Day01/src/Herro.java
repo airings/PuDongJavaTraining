628231127931231de2a11002c836df1e6d99dd35
airings <zz_zxy@163.com>
2015-08-22 11:42:26 +0800

    test

---
+++ Day01/src/Herro.java
@@ -0,0 +1,22 @@
+
+public class Herro {
+	private String name;
+	private String pwd;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
